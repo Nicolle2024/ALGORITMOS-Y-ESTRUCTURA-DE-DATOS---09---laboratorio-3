2b6bc582b8678ac50ce6e1245f324cbd09a42964
Nicolle2024 <nicolle.castilla@gmail.com>
2026-03-29 20:10:11 -0500

    Add logic to count duplicate elements in array
    Implemented nested loop to count duplicates in array.

--- Ejercicios/ConteoI.java
+++ Ejercicios/ConteoI.java
@@ -3,7 +3,14 @@ public class ConteoI {
     public static int conteoI(int[] v) { 
         int conteo = 0; // O(1) 
         for (int i = 0; i < v.length - 1; i++) {        // O(n) 
-           falta completar
+           for (int j = i + 1; j < v.length; j++) {    // O(n) 
+                if (v[i] == v[j]) {                    // O(1) 
+                    conteo++; 
+                } 
+            } 
+        } 
+        return conteo; // O(1) 
+    }
 
     public static void main(String[] args) { 
         int[] v = {1, 2, 1, 2}; 
